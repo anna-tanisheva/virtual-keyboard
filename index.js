116724a4c804987ec26eef65038bc911ed0385a3
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-04-30 15:39:17 +0300

    feat: 1.create project structure, partally add keys 2. Add event listeners for keys, describe some special keys (such as Delete) behaviour

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const HTML = document.querySelector('html');
 const HEAD = document.querySelector('head');
 const BODY = document.querySelector('body');
 
-let template = `<header>
+let template = `<header class="header">
 <div class="container">
-	<h1>Virtual Keyboard</h1>
+	<h1 class="main-header">Virtual Keyboard</h1>
 </div>
 </header>
 <main class="main">
 <div class="container">
-	<textarea name="output" id="output" cols="30" rows="10" class="output"></textarea>
+	<textarea name="output" id="output" cols="30" rows="10" class="output" autofocus></textarea>
 
 	<div class="keyboard-wrapper"></div>
 </div>
@@ -28,6 +28,151 @@ let template = `<header>
 </div>
 </footer>`
 BODY.innerHTML = template;
+
+//key template
+const KEYBOARD = document.querySelector('.keyboard-wrapper');
+let serverKeys = ['Tab', 'Backspace', 'Del'];
+
+function createKey(value, shiftValue, keyCode, inner) {
+	let key = document.createElement('div');
+	key.classList.add('key');
+	key.setAttribute('data-key', keyCode);
+	key.setAttribute('data-value', value);
+	key.setAttribute('data-shift-value', shiftValue);
+	if (inner.length > 1) {
+		inner.forEach((elem) => {
+			if (elem === inner[0]) {
+				let first = document.createElement('span');
+				first.innerHTML += elem;
+				first.classList.add('inner-first');
+				first.classList.add('inner');
+				key.append(first);
+			} else {
+				let second = document.createElement('span');
+				second.innerHTML += elem;
+				second.classList.add('inner-second');
+				second.classList.add('inner');
+				key.append(second);
+			}
+		})
+	} else {
+		key.classList.add('span2');
+		if (key.getAttribute('data-value') === 'Del') {
+			key.classList.add('span1');
+		}
+		// console.log(inner[0])
+		if (serverKeys.includes(inner[0])) {
+			key.classList.add('server')
+		}
+		let first = document.createElement('span');
+		first.innerHTML += inner[0];
+		first.classList.add('inner');
+		key.append(first);
+	}
+
+
+	return key;
+}
+
+
+//fetch keys
+let data = []
+const QUERY_EN = {
+	load: `data/keyboard_en.json`,
+	user: null
+};
+async function fetchKeys(query) {
+	const url = query.load;
+	const res = await fetch(url);
+	const data = await res.json();
+	for (let item of data) {
+		let key = createKey(item.value, item.shiftValue, item.keyCode, item.inner);
+		KEYBOARD.append(key)
+	}
+}
+window.addEventListener('load', () => {
+	fetchKeys(QUERY_EN)
+})
+
+//event listeners on virtual keyboard
+// let keys = KEYBOARD.querySelectorAll('.key');
+
+//EL for always focused textarea
+const OUTPUT = document.querySelector('.output')
+OUTPUT.addEventListener('blur', () => {
+	OUTPUT.focus()
+})
+
+KEYBOARD.addEventListener('click', (e) => {
+	//EL for regular keys
+	let caretPos = OUTPUT.selectionStart;
+
+	if (e.target.classList.contains('key') && !e.target.classList.contains('server')) {
+		let clickedKey = e.target;
+		let dataValue = clickedKey.getAttribute('data-value');
+		let dataShiftValue = clickedKey.getAttribute('data-shift-value');
+		if (caretPos === OUTPUT.value.length) {
+			OUTPUT.value += dataValue
+		} else if (caretPos < OUTPUT.value.length) {
+			OUTPUT.value = OUTPUT.value.slice(0, caretPos) + dataValue + OUTPUT.value.slice(caretPos)
+			OUTPUT.selectionStart = caretPos + 1
+			OUTPUT.selectionEnd = caretPos + 1
+		}
+	} else if (e.target.classList.contains('inner') && !e.target.parentNode.classList.contains('server')) {
+		let clickedKey = e.target.parentNode;
+		let dataValue = clickedKey.getAttribute('data-value');
+		let dataShiftValue = clickedKey.getAttribute('data-shift-value');
+		if (caretPos === OUTPUT.value.length) {
+			OUTPUT.value += dataValue
+		} else if (caretPos < OUTPUT.value.length) {
+			OUTPUT.value = OUTPUT.value.slice(0, caretPos) + dataValue + OUTPUT.value.slice(caretPos)
+			OUTPUT.selectionStart = caretPos + 1
+			OUTPUT.selectionEnd = caretPos + 1
+		}
+	}
+
+	//EL for serve keys
+	if (e.target.classList.contains('key') && e.target.classList.contains('server')) {
+		let clickedKey = e.target;
+		let dataValue = clickedKey.getAttribute('data-value');
+
+		if (dataValue === 'Backspace') {
+			OUTPUT.value = OUTPUT.value.slice(0, -1)
+		}
+
+		if (dataValue === 'Tab') {
+			OUTPUT.value += `    `
+		}
+		if (dataValue === 'Del') {
+			let caretPos = OUTPUT.selectionStart;
+			if (caretPos < OUTPUT.value.length) {
+				OUTPUT.value = OUTPUT.value.split('').filter((data, idx) => idx !== caretPos).join('');
+				OUTPUT.selectionStart = caretPos
+				OUTPUT.selectionEnd = caretPos
+			}
+		}
+	} else if (e.target.classList.contains('inner') && e.target.parentNode.classList.contains('server')) {
+		let clickedKey = e.target.parentNode;
+		let dataValue = clickedKey.getAttribute('data-value');
+
+		if (dataValue === 'Backspace') {
+			OUTPUT.value = OUTPUT.value.slice(0, -1)
+		}
+
+		if (dataValue === 'Tab') {
+			OUTPUT.value += `    `
+		}
+		if (dataValue === 'Del') {
+			let caretPos = OUTPUT.selectionStart;
+			if (caretPos < OUTPUT.value.length) {
+				OUTPUT.value = OUTPUT.value.split('').filter((data, idx) => idx !== caretPos).join('');
+				OUTPUT.selectionStart = caretPos
+				OUTPUT.selectionEnd = caretPos
+			}
+		}
+	}
+	console.log()
+})
 
 
 
